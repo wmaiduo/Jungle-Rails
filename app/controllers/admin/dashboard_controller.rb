025b88479e47d75e19651ced9fd7e590f48a03a4
Mai Duo Wu <wmaiduo@gmail.com>
2021-10-14 07:39:20 +0000

    basic HTTP Authentication added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  puts "___________________________"
+  puts ENV['USERNAME']
+  puts ENV["PASSWORD"]
+  puts "end"
+
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV["PASSWORD"]
+  
   def show
   end
 end
