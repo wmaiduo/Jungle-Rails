b6045b6a9ea7fe080836a565da4e53d9a06ad75d
Mai Duo Wu <wmaiduo@gmail.com>
2021-10-14 07:54:46 +0000

    added admin dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  puts "___________________________"
-  puts ENV['USERNAME']
-  puts ENV["PASSWORD"]
-  puts "end"
-
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV["PASSWORD"]
   
   def show
+    @categories_count = Category.count.to_i
+    @products_count = Product.count.to_i
   end
 end
