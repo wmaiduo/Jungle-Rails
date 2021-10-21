faa0b1ef4932b586bb82dc240c32e53153a84d9d
Mai Duo Wu <wmaiduo@gmail.com>
2021-10-21 03:10:19 +0000

    add test for product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
+RSpec.feature "Visitor navigates to a single product", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -21,10 +19,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
+    page = first('.btn-default').click
+
+    puts page
     # DEBUG
-    # save_screenshot
+
+    sleep(0.1)
+    save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+
   end
 end
